8bfbb2d78b45e640c8a7b4b093c13afcad13b88f
Eric Milan <emilan@etmilan.org>
2017-05-20 21:06:28 -0400

    Got mac_convertor script working. Didn't add the clipboard functionally yet. Added empty README.md. Need to add setup.py later

--- mac_convertor/mac_convertor.py
+++ mac_convertor/mac_convertor.py
@@ -4,12 +4,33 @@
 
 import re
 
+# Get mac address from user
 print("Enter a mac address")
 original_mac = input()
 
-colon_dash_notation_reg = re.compile('^([0-9A-Fa-f]{2}[:-]){5}([0-9A-Fa-f]{2})')
+# Regexs for mac notation
+colon_dash_notation_reg = re.compile('^(([0-9A-Fa-f]{2}[:-]){5})([0-9A-Fa-f]{2})')
 hp_notation_reg = re.compile('^([0-9-A-F-a-f]{4}-){2}([0-9A-Fa-f]{4})')
 
-if original_mac == colon_dash_notation_reg.search(original_mac):
-    reg_obj = colon_dash_notation_reg.search(original_mac)
-    hp_mac = 
+# Check if mac from users is a type of standard notation with dashes or colons, or the notation HP uses.
+# for thier switches
+if colon_dash_notation_reg.match(original_mac):
+    print("Mac is colon dash notation")
+    print("Will print out HP notation below and copy to clipboard")
+    split_list = re.split(':|-', original_mac)
+    hp_mac = split_list[0] + split_list[1] + '-' + split_list[2] + split_list[3] + '-' + split_list[4] + split_list[5]
+    print(hp_mac)
+elif hp_notation_reg.match(original_mac):
+    print("Mac is HP notation")
+    print("Will print out standard notation below and copy to clipboard")
+    split_list = re.split('-', original_mac)
+    first_string = split_list[0]
+    second_string = split_list[1]
+    thrid_string = split_list[2]
+    octet_1 = first_string[0] + first_string[1] + ':'
+    octet_2 = first_string[2] + first_string[3] + ':'
+    octet_3 = second_string[0] + second_string[1] + ':'
+    octet_4 = second_string[2] + second_string[3] + ':'
+    octet_5 = thrid_string[0] + thrid_string[1] + ':'
+    octet_6 = thrid_string[2] + thrid_string[3]
+    print(octet_1 + octet_2 + octet_3 + octet_4 + octet_5 + octet_6)
