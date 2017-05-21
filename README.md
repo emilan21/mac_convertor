f53210f468fb4e6b1c6089ae3594533674e8396f
Eric Milan <emilan@etmilan.org>
2017-05-20 21:20:27 -0400

    Added setup.py and updated README.md

---
+++ README.md
@@ -0,0 +1,5 @@
+##### Mac Convertor Project
+
+## Summary
+
+Converts standard notation mac address to HP notation mac address.
---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Basic setup.py file
+
+from distutils.core import setup
+
+setup(name='mac_convertor',
+        description='Converts mac address from standard notation to HP notation',
+        version='0.1',
+        author='Eric Milan',
+        author_email='emilan.etmilan.org',
+        packages=['mac_convertor'],
+)
